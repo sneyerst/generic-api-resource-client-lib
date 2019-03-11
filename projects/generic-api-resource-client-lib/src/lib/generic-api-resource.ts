a1f93ab39d364e08bbdc003b04ba1cf73e30aa47
Tom Sneyers <tom.sneyers@vito.be>
2019-03-11 09:46:39 +0100

    added support for api namespace in list

--- projects/generic-api-resource-client-lib/src/lib/generic-api-resource.ts
+++ projects/generic-api-resource-client-lib/src/lib/generic-api-resource.ts
@@ -18,6 +18,7 @@ export class GenericApiResource {
   _enableNew: boolean;
   _enableEdit: boolean;
   _enableDestroy: boolean;
+  _namespace: string;
 
   @Input() set apiUrl(apiUrl: string) { this._apiUrlObservable.next(apiUrl); }
   @Input() set resourceNameSingular(resourceNameSingular: string) {
@@ -42,22 +43,24 @@ export class GenericApiResource {
   @Input() set enableNew(enableNew: boolean) { this._enableNew = enableNew; }
   @Input() set enableEdit(enableEdit: boolean) { this._enableEdit = enableEdit; }
   @Input() set enableDestroy(enableDestroy: boolean) { this._enableDestroy = enableDestroy; }
+  @Input() set namespace(namespace: string) { this._namespace = namespace; }
 
   get apiUrlObservable(): BehaviorSubject<string> { return this._apiUrlObservable; }
   get apiUrl(): string { return this._apiUrlObservable.getValue(); }
   get resourceNameSingular(): string { return this._resourceNameSingular; }
   get resourceNamePlural(): string { return this._resourceNamePlural == null ? this._resourceNameSingular + 's' : this._resourceNamePlural; }
-  get resourceIndexUri(): string { return this._resourceIndexUri == null ? `/${this.resourceNamePlural}` : this._resourceIndexUri; }
-  get resourceShowUri(): string { return this._resourceShowUri == null ? `/${this.resourceNamePlural}/:id` : this._resourceShowUri; }
-  get resourceNewUri(): string { return this._resourceNewUri == null ? `/${this.resourceNamePlural}/new` : this._resourceNewUri; }
-  get resourceCreateUri(): string { return this._resourceCreateUri == null ? `/${this.resourceNamePlural}` : this._resourceCreateUri; }
-  get resourceEditUri(): string { return this._resourceEditUri == null ? `/${this.resourceNamePlural}/edit` : this._resourceEditUri; }
-  get resourceUpdateUri(): string { return this._resourceUpdateUri == null ? `/${this.resourceNamePlural}/:id` : this._resourceUpdateUri; }
-  get resourceDestroyUri(): string { return this._resourceDestroyUri == null ? `/${this.resourceNamePlural}/:id` : this._resourceDestroyUri; }
+  get resourceIndexUri(): string { return this._resourceIndexUri == null ? `${this.namespace}${this.resourceNamePlural}` : this._resourceIndexUri; }
+  get resourceShowUri(): string { return this._resourceShowUri == null ? `${this.namespace}${this.resourceNamePlural}/:id` : this._resourceShowUri; }
+  get resourceNewUri(): string { return this._resourceNewUri == null ? `${this.namespace}${this.resourceNamePlural}/new` : this._resourceNewUri; }
+  get resourceCreateUri(): string { return this._resourceCreateUri == null ? `${this.namespace}${this.resourceNamePlural}` : this._resourceCreateUri; }
+  get resourceEditUri(): string { return this._resourceEditUri == null ? `${this.namespace}${this.resourceNamePlural}/edit` : this._resourceEditUri; }
+  get resourceUpdateUri(): string { return this._resourceUpdateUri == null ? `${this.namespace}${this.resourceNamePlural}/:id` : this._resourceUpdateUri; }
+  get resourceDestroyUri(): string { return this._resourceDestroyUri == null ? `${this.namespace}${this.resourceNamePlural}/:id` : this._resourceDestroyUri; }
   get enableIndex(): boolean { return this._enableIndex == null ? true : this._enableIndex; }
   get enableShow(): boolean { return this._enableShow == null ? true : this._enableShow; }
   get enableNew(): boolean { return this._enableNew == null ? true : this._enableNew; }
   get enableEdit(): boolean { return this._enableEdit == null ? true : this._enableEdit; }
   get enableDestroy(): boolean { return this._enableDestroy == null ? true : this._enableDestroy; }
+  get namespace(): string { return this._namespace == null ? '/' : `/${this._namespace}`; }
 
 }
