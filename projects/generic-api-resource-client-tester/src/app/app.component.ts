06660352b4e5bd8d2a0a255b6a23f4ce18acf8af
Tom Sneyers <tom@sneyers-consultancy.be>
2019-04-12 09:31:33 +0200

    removed \n

--- projects/generic-api-resource-client-tester/src/app/app.component.ts
+++ projects/generic-api-resource-client-tester/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<p>Welcome to the <strong>generic-api-resource-client-tester project</strong></p>\n',
+  template: '<p>Welcome to the <strong>generic-api-resource-client-tester project</strong></p>',
   styleUrls: []
 })
 export class AppComponent {
