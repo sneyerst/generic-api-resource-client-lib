4836f90b74b375dbc2d33cb1109ddac06c3cd445
Tom Sneyers <tom@sneyers-consultancy.be>
2019-08-31 08:13:32 +0200

    files_async file downloading

--- projects/generic-api-resource-client-lib/src/lib/fields/files_async.component.ts
+++ projects/generic-api-resource-client-lib/src/lib/fields/files_async.component.ts
@@ -8,7 +8,7 @@ import {DomSanitizer} from "@angular/platform-browser";
   selector: 'generic-api-resource-files-async',
   template: `
     <div [formGroup]="parentFormGroup">
-      
+        
       <ul class="file-list" formArrayName="{{formComponent.name}}"
           *ngFor="let item of getControlsFor(formComponent.name); let i = index;">
         <li [formGroupName]="i">
@@ -18,9 +18,7 @@ import {DomSanitizer} from "@angular/platform-browser";
           </mat-form-field>
           &nbsp;
           <ng-container *ngIf="item['value']['id']">
-            <button mat-raised-button (click)="downloadFile(formComponent['url'].replace(':id', item['value']['id']), item['value']['id'])">Download</button>&nbsp;
-            <a target="_blank" id="{{'downloadLink-' + item['value']['id']}}" [href]="downloadURL" style="display: none;">Hidden download link</a>
-
+            <button mat-raised-button (click)="downloadFile(formComponent['url'].replace(':id', item['value']['id']), item['value']['label'])">Download</button>&nbsp;
             <mat-checkbox class="file-destroy" formControlName="_destroy">Delete</mat-checkbox>
           </ng-container>
 
@@ -35,9 +33,6 @@ import {DomSanitizer} from "@angular/platform-browser";
 })
 export class FilesAsyncComponent extends Field {
 
-  downloadURL;
-  clickedElement;
-
   @Input()
   uploadUrl: string = 'http://localhost:3000';
 
@@ -48,17 +43,36 @@ export class FilesAsyncComponent extends Field {
     super();
   }
 
-  downloadFile(url, item_id) {
+  downloadFile(url, filename) {
     this.filesService.download(url).subscribe((data: any) => {
+      // It is necessary to create a new blob object with mime-type explicitly set
+      // otherwise only Chrome works like it should
 
-      this.downloadURL = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(data));
+      let contentType = 'application/pdf';
+      if(filename.endsWith('.xlsx')) {
+        contentType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+      }
 
-      console.log(this.downloadURL);
-      let element: HTMLElement = document.getElementById('downloadLink-' + item_id) as HTMLElement;
+      const newBlob = new Blob([(data)], { type: contentType });
 
-      element.removeAttribute('style');
-      //setTimeout("this.clickedElement.click();", 1000);
-      //element.click();
+      // IE doesn't allow using a blob object directly as link href
+      // instead it is necessary to use msSaveOrOpenBlob
+      if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveOrOpenBlob(newBlob);
+        return;
+      }
+
+      // For other browsers:
+      // Create a link pointing to the ObjectURL containing the blob.
+      // const downloadURL = URL.createObjectURL(newBlob);
+      // window.open(downloadURL);
+
+      const downloadURL = window.URL.createObjectURL(data);
+      let link = document.createElement('a');
+      link.href = downloadURL;
+      link.download = filename;
+      link.click();
+
     });
   }
 
