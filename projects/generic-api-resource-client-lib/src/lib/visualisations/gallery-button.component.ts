d2138ccb121acd4cc3469a0cee8b533ae63f8a6f
Tom Sneyers <tom@sneyers-consultancy.be>
2019-09-17 10:14:36 +0200

    added imports for photoswipe

--- projects/generic-api-resource-client-lib/src/lib/visualisations/gallery-button.component.ts
+++ projects/generic-api-resource-client-lib/src/lib/visualisations/gallery-button.component.ts
@@ -1,12 +1,16 @@
 import {Component, Input} from '@angular/core';
 import {Router} from '@angular/router';
 
+import * as PhotoSwipe from 'photoswipe';
+import * as PhotoSwipeUI_Default from "photoswipe/dist/photoswipe-ui-default";
+
 @Component({
   selector: 'generic-api-resource-gallery-button',
-  template: `    
-    <button mat-raised-button color="primary" (click)="openGallery(_item); $event.stopPropagation()">
-      View in gallery
-    </button>
+  template: `
+      <button *ngIf="_item.length > 0" mat-raised-button color="primary"
+              (click)="openGallery(_item); $event.stopPropagation()">
+          View in gallery
+      </button>
   `,
   styleUrls: [],
   providers: []
@@ -15,9 +19,7 @@ export class GalleryButtonComponent {
 
   _item: any[];
 
-  constructor(
-    private router: Router
-  ) {
+  constructor(private router: Router) {
   }
 
   @Input()
@@ -25,8 +27,19 @@ export class GalleryButtonComponent {
     this._item = item;
   }
 
- openGallery(item) {
-    console.log(item);
- }
+  openGallery(items) {
+    let pswpElement = document.querySelectorAll('.pswp')[0] as HTMLElement;
+
+    // define options (if needed)
+    let options = {
+      // optionName: 'option value'
+      // for example:
+      index: 0 // start at first slide
+    };
+
+    // Initializes and opens PhotoSwipe
+    let gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+  }
 
 }
