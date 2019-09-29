a4ee1d5808abdf2e8976201b3f6de93144392393
Tom Sneyers <tom@sneyers-consultancy.be>
2019-09-29 14:49:32 +0200

    include status-icons

--- projects/generic-api-resource-client-lib/src/lib/visualisations/status-icons.component.ts
+++ projects/generic-api-resource-client-lib/src/lib/visualisations/status-icons.component.ts
@@ -3,13 +3,21 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'generic-api-resource-status-icons',
   template: `
-   status icons <mat-icon aria-hidden="false" aria-label="Example home icon">home</mat-icon>
+    <div class="status-icon" *ngFor="let item of _items; let i=index">
+        <div class="index-value">{{i}}</div>
+        <mat-icon aria-hidden="false" aria-label="Truck">
+            {{_icon}}
+        </mat-icon>
+        <div class="percent-value">100%</div>
+    </div>
+   
   `,
   styleUrls: [],
   providers: []
 })
 export class StatusIconsComponent {
 
+  _icon: string = 'local_shipping';
   _items: any[];
 
   @Input()
